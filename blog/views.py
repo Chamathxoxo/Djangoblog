283b07d4307c07261a3865973e3fb958240b25da
Chamathxoxo <chamathxo@gmail.com>
2026-09-11 15:14:35 +0900

    Update static files, base template, and contact page

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
 
-# Create your views here.
+def home(request):
+    return render(request, "blog/home.html", {"title": "DjangoBlog Homepage"})
+
+def about(request):
+    return render(request, "blog/about.html", {"title": "Us"})
+
+def contact(request):
+    return render(request, "blog/contact.html", {"title": "Us"})
+
+
